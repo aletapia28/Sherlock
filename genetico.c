dcd4ee77a75acb2208628530cd020487021e121f
Alejandro Tapia <aletapiab28@gmail.com>
2018-06-05 17:54:20 -0600

    Algoritmo genetico

---
+++ genetico.c
@@ -0,0 +1,24 @@
+struct Individuo{
+    int genome ;
+    int score;
+
+}Individuo;
+
+/*
+struct Individuo createIndividuo (int fenotipo){
+
+	struct Individuo indiv;
+	indiv.genome = fenotipo;
+	return indiv;
+};*/
+//--------------Cosas Por hacer--------------------------
+// Genotipo
+//Poblacion
+//% Mutación
+//Cruce punto fijo o variable
+//Fitness
+
+
+int fitnessFuntion();
+void createPoblation(){ int a =0; };
+
